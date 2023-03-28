f28cf50631aefcfee2e9790d4d180dd83c1a97eb
Artur Tenorio <artur.tenorio@swisscom.com>
2023-03-28 10:33:29 +0200

    create the base of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './components/people/people.component';
+import { PlanetComponent } from './components/planet/planet.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PeopleComponent, PlanetComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
