b685c47813a298d709d6ecc1defe4b2410dda6a5
Artur Tenorio <artur.tenorio@swisscom.com>
2023-03-28 15:11:00 +0200

    add routes

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { People } from 'src/app/swapi.service';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
-  styleUrls: ['./people.component.scss']
+  styleUrls: ['./people.component.scss'],
 })
 export class PeopleComponent {
-
+  @Input() peopleList!: People[];
 }
--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,9 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, combineLatest, map } from 'rxjs';
+
+export interface People {
+  name: string;
+  gender: string;
+}
+
+export interface Planet {
+  name: string;
+  population: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwapiService {
+  readonly baseUrl = 'https://swapi.dev/api';
+  readonly peopleUrl = `${this.baseUrl}/people`;
+  readonly planetsUrl = `${this.baseUrl}/planets`;
+
   constructor(private httpClient: HttpClient) {}
+
+  getPeopleList(): Observable<People[]> {
+    return combineLatest([this.getPeople(1), this.getPeople(2)]).pipe(
+      map(([people1, people2]) => [people1, people2])
+    );
+  }
+
+  getPlanetsList(): Observable<Planet[]> {
+    return combineLatest([this.getPlanets(1), this.getPlanets(2)]).pipe(
+      map(([plane1, plane2]) => [plane1, plane2])
+    );
+  }
+
+  private getPeople(id: number): Observable<People> {
+    return this.httpClient.get<People>(this.peopleUrl + `/${id}`);
+  }
+
+  private getPlanets(id: number): Observable<Planet> {
+    return this.httpClient.get<Planet>(this.planetsUrl + `/${id}`);
+  }
 }
